eeb37467b2f721e9f137a22c037b0ac8a56467bd
AndreDavidRodriguezArtavia <115605787+AndreDavidRodriguezArtavia@users.noreply.github.com>
2022-11-24 15:16:11 -0600

    avances jueves proyecto final

---
+++ Entidades/Datos.cs
@@ -0,0 +1,12 @@
+﻿
+namespace Entidades
+{
+    public class Datos
+    {
+        //Datos: secuencia computadora, secuencia del jugador
+        //nivel actual(registro)
+        public int[] secuenciaComputadora;
+        public int[] secuenciaJugador;
+        public int nivelActual;
+    }
+}
---
+++ LogicadeJuego/Logica.cs
@@ -0,0 +1,34 @@
+﻿using Entidades;
+using System.Security.Cryptography.X509Certificates;
+
+namespace LogicadeJuego
+{
+    public class Logica
+    {
+        //Propiedades:
+
+        public Datos datosJuego;
+
+        // Metodo constructor
+        public Logica()
+        {
+            datosJuego = new Datos();
+            datosJuego.secuenciaJugador = new int[10];
+            datosJuego.secuenciaComputadora = new int[10];
+            datosJuego.nivelActual = 3;
+        }
+            
+       
+        // Metodo llenar la secuencia de la computadora con numeros aleatorio
+      
+        public void llenarSecuencia()
+        {
+            
+        }
+
+        // metodo para aumentar el nivel del juego 
+        // metodo de gane y perdida  
+        // metodo que compara casilla por casilla la secuencia de la computadora con la respuesta del jugador para saber si es correcta
+
+    }
+}
