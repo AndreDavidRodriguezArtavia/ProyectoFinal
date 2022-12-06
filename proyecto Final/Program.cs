be90479abf280392251d48f3f4da5422a17ad311
AndreDavidRodriguezArtavia <115605787+AndreDavidRodriguezArtavia@users.noreply.github.com>
2022-12-06 16:55:30 -0600

    arreglos en portada y sus opciones

--- proyecto Final/Program.cs
+++ proyecto Final/Program.cs
@@ -4,81 +4,15 @@ namespace ProyectoFinalAndreRodriguez
 {
     internal class Program
     {
-        // metodo para enseñar la portada del juego 
-
-        static void MostrarlaPortada()
-        {
-            Console.Clear();
-            Console.WriteLine("Bienvenidos a memorium");
-            Console.WriteLine("prueba tu capacidad de memorizar");
-            Console.ReadLine();
-        }
-
-
-        // metodo para mostrar las instruciones 
-
-        static void Instruciones()
-        {
-            Console.WriteLine("1- Al empezar el juego se le mostrara al inicio una secuenica la cual le aparecera por un tempo definido, cuando este tiempo concluya se le quitara de la vista la secuencia");
-            Console.WriteLine("una vez no se le muestre la secuencia sele solicitara ingresar la secuencia vista anteriormente de manera correcta siendo asi que se avanzara en al siguiente nivel");
-            Console.WriteLine("de ser el caso contrario perdera el juego.");
-            Console.WriteLine("2- Al avanzar de nivel la secuencia durara menos tiempo en ser visibli y en ciertos casos se llegara a ver de una manera parpadeada");
-            Console.WriteLine("3- El juego le indicara cuando a pasado el nivel del mismo caso sucedera con terminar o perder el juego");
-            Console.ReadLine();
-        }
-
-        // Metodo para enseñar los creditos 
-
-        static void Creditos()
-        {
-            Console.WriteLine("Creado por André Rodríguez");
-            Console.WriteLine("Arte ASCII por: ascii-art-generator.org");
-            Console.ReadLine();
-        }
-
-        //Metodo para poder iniciar el juego 
-        static void Comienzodeljuego()
-        {
-
-        }
-
-        // metodo para que se enseñen las opciones 
-        static void Opciones()
-        {
-            Console.WriteLine("1- Empezar juego");
-            Console.WriteLine("2- Instruciones del juego");
-            Console.WriteLine("3- Creditos");
-            Console.WriteLine("4- salir");
-            Console.WriteLine("Digite su opcion escogida:");
-            Console.ReadLine();
-        }
-
-        // Funcion que transforma el texto y regresa por el error a -1
-
-        static int CapturarRespuesta()
-        {
-            try
-            {
-                string Textoqueescribioelusuario;
-                Textoqueescribioelusuario = Console.ReadLine();
-                return int.Parse(Textoqueescribioelusuario);
-            }
-            catch
-            {
-                Console.WriteLine("error al digitar su selección ");
-                Console.WriteLine("Solo se pude digar un numero del 1 al 4 con sus respectivas aclaraciones de las opciones");
-                return -1;
-            }
-        }
         static void Main(string[] args)
         {
-            int SelecciondelUsuario = 0;
+            int SeleccionDelUsuario = 0;
 
-            // Ciclo (Gameloop)
-
-            while (SelecciondelUsuario != 4)
+            // Cliclo (Gameloop)
+            while (SeleccionDelUsuario != 4)
             {
-                //Opción para enseñar la portada  
+
+                //Opción para enseñar la portada 
 
                 MostrarlaPortada();
 
@@ -88,39 +22,101 @@ namespace ProyectoFinalAndreRodriguez
 
                 //Manera para obtener la escogencia del jugador 
 
-                SelecciondelUsuario = CapturarRespuesta();
+                SeleccionDelUsuario = CapturarNumero();
 
-                if (SelecciondelUsuario == 1)
+
+                if (SeleccionDelUsuario == 1)
                 {
                     // cuando se escoge 1 se comienza el juego
-
                     Console.WriteLine("Has escogido iniciar el juego seras enviado al nivel de inicio del juego");
                     Console.ReadLine();
+                    Comienzodeljuego();
                 }
-                else if (SelecciondelUsuario == 2)
+                else if (SeleccionDelUsuario == 2)
                 {
-                    // cuando se escoge 2 se mostraran las instruciones 
-
-                    Instruciones();
-                }
-                else if (SelecciondelUsuario == 3)
+                    // cuando se escoge 2 se mostraran las instruciones
+                    Instrucciones();
+                } 
+                else if (SeleccionDelUsuario == 3)
                 {
-                    // cuando se escogue 3 se mostraran los creditos 
-
+                    // cuando se escogue 3 se mostraran los creditos
                     Creditos();
                 }
-                else if (SelecciondelUsuario == 4)
+                else if (SeleccionDelUsuario == 4)
                 {
                     // cuando el jugador escoge 4 sera para salir del juego
                     Console.WriteLine("has escogido salir del juego");
                     Console.ReadLine();
                 }
-
-
-
-
             }
-        } 
+        }
+
+        // metodo para mostrar las instruciones
+        static public void Instrucciones()
+        {
+            Console.WriteLine("Instruciones del juego");
+            Console.WriteLine("1- Al empezar el juego se le mostrara al inicio una secuenica la cual le aparecera por un tempo definido, cuando este tiempo concluya se le quitara de la vista la secuencia");
+            Console.WriteLine("una vez no se le muestre la secuencia sele solicitara ingresar la secuencia vista anteriormente de manera correcta siendo asi que se avanzara en al siguiente nivel");
+            Console.WriteLine("de ser el caso contrario perdera el juego.");
+            Console.WriteLine(" Al avanzar de nivel la secuencia durara menos tiempo en ser visible y en ciertos casos se llegara a ver de una manera parpadeada");
+            Console.WriteLine(" 3- El juego le indicara cuando a pasado el nivel del mismo caso sucedera con terminar o perder el juego");
+            Console.ReadLine();
+        }
+
+        // Metodo para enseñar los creditos 
+
+        static public void MostrarlaPortada()
+        {
+            Console.Clear();
+            Console.WriteLine("Bienvenidos a memorium");
+            Console.WriteLine("prueba tu capacidad de memorizar");
+            Console.ReadLine();
+        }
+
+        // Metodo para enseñar los creditos
+
+        static public void Creditos()
+        {
+            Console.WriteLine("Creados por: André Rodríguez Artavia");
+            Console.WriteLine("Arte ASCII por: ascii-art-generator.org");
+            Console.ReadLine();
+        }
+
+        //Metodo para poder iniciar el juego
+
+        static public void Comienzodeljuego()
+        {
+
+        }
+
+        // Funcion que transforma el texto y regresa por el error a -1
+
+        static public int CapturarNumero()
+        {
+            try
+            {
+                string textoEscritoPorElUsuario;
+                textoEscritoPorElUsuario = Console.ReadLine();
+                return int.Parse(textoEscritoPorElUsuario);
+            }
+            catch
+            {
+                Console.WriteLine("Error al escrbir");
+                Console.WriteLine("solo se aceptan numeros del 1 al 4 ");
+                Console.ReadLine();
+                return -1;
+            }
+        }
+
+        // metodo para que se enseñen las opciones 
+
+        static public void Opciones()
+        { 
+            Console.WriteLine("1- Empezar juego");
+            Console.WriteLine("2- Instruciones del juego");
+            Console.WriteLine("3- Creditos");
+            Console.WriteLine("4- salir");
+            Console.WriteLine("Digite su opcion escogida:");
+        }
     }
-}   
-    
+}
